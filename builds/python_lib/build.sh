5af453debfd88b277e7f83f3b6cfd849428b82da
Mads D. Kristensen <madsdk@gmail.com>
2010-12-08 12:44:38 +0100

    Initial commit - largely a copy of python_client_lib

---
+++ builds/python_lib/build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+LIBDIR=../../libs/
+
+# Check for version number
+if [ "$1" = "" ]; then
+	echo "Usage: build.sh version_number";
+	exit 1
+else
+	VERSION="-$1"
+fi
+BUILDDIR=presence_lib$VERSION
+
+# Create the $BUILDDIR dir
+if [ -d $BUILDDIR ]; then 
+	rm -rf $BUILDDIR;
+fi
+mkdir -p $BUILDDIR/presence/packed_data
+
+# Copy python files into the $BUILDDIR dir.
+cp $LIBDIR/python_common/src/*.py $BUILDDIR/presence
+cp $LIBDIR/python_common/src/packed_data/*.py $BUILDDIR/presence/packed_data
+cp $LIBDIR/python/src/presence/*.py $BUILDDIR/presence
+cp setup.py $BUILDDIR/
+sed -i "" s/VERSION/$VERSION/ $BUILDDIR/setup.py
+
+tar cfz $BUILDDIR.tar.gz $BUILDDIR
+
+exit 0
---
+++ builds/python_lib/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Presence Library',
+      version='VERSION',
+      description='The full fledged Presence library.',
+      author='Mads D. Kristensen',
+      author_email='madsk@cs.au.dk',
+      url='https://github.com/madsdk/presence',
+      packages=['presence', 'presence.packed_data']
+)
